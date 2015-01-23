3a7868f9e1339df709cc7b632383beeb7482e54d
James Conroy-Finn <james@logi.cl>
2015-01-23 14:03:25 +0000

    JSHint extension JS

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,10 +1,18 @@
-$.ajax({
-  url: 'https://localhost:3131/'+location.hostname.replace(/^www\./,'')+'.js',
-  dataType: 'text',
-  success: function(d){
-    $(function(){ eval(d) })
-  },
-  error: function(){
-    console.log('no dotjs server found at localhost:3131')
+(function($) {
+  'use strict';
+
+  function getURL(s) {
+    return 'https://localhost:3131/' + s + '.js';
   }
-})
+
+  $.ajax({
+    url: getURL(location.hostname.replace(/^www\./, '')),
+    dataType: 'text',
+    success: function (data) {
+      $(function () { eval(data); });
+    },
+    error: function () {
+      console.error('No dotjs server found at https://localhost:3131.');
+    }
+  });
+})(jQuery);
